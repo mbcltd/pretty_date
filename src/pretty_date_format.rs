ccc28441b0353ca6bb6fc5f9bf0bca9386f4180a
David Morgan-Brown <david@morgan-brown.com>
2023-01-03 12:34:34 +0000

    Implement formatting rules with a first simple one.

--- src/pretty_date_format.rs
+++ src/pretty_date_format.rs
@@ -3,13 +3,14 @@ use chrono::{Local, NaiveDateTime};
 use crate::pretty_date_formatter::PrettyDateFormatter;
 
 pub struct PrettyDateFormat<'a> {
-    // pub rules: Vec<Box<dyn PrettyDateFormatRule>>,
+    pub rules: Vec<Box<dyn PrettyDateFormatRule>>,
     pub default_format: &'a str,
 }
 
 impl<'a> PrettyDateFormat<'a> {
     pub fn default() -> PrettyDateFormat<'a> {
         PrettyDateFormat {
+            rules: vec![Box::new(JustNowPrettyDateFormatRule::new())],
             default_format: "%-e %B %Y, %H:%M",
         }
     }
@@ -19,11 +20,39 @@ impl<'a> PrettyDateFormat<'a> {
     }
 
     pub fn format_pretty_with_reference(&self, date: &NaiveDateTime, reference_date: &NaiveDateTime) -> String {
-        date.format_with_items(StrftimeItems::new(self.default_format)).to_string()
+        let matching_rule =
+            self
+                .rules
+                .iter()
+                .find(|&the_rule| the_rule.does_match(date, reference_date));
+
+        match matching_rule {
+            Some(rule) => date.format_with_items(rule.format()).to_string(),
+            None => date.format_with_items(StrftimeItems::new(self.default_format)).to_string()
+        }
     }
 }
 
 pub trait PrettyDateFormatRule {
-    fn matches(&self, date: &NaiveDateTime, reference_date: &NaiveDateTime) -> bool;
+    fn does_match(&self, date: &NaiveDateTime, reference_date: &NaiveDateTime) -> bool;
     fn format<'a>(&self) -> StrftimeItems<'a>;
+}
+
+struct JustNowPrettyDateFormatRule {}
+
+impl JustNowPrettyDateFormatRule {
+    pub fn new() -> JustNowPrettyDateFormatRule {
+        JustNowPrettyDateFormatRule {}
+    }
+}
+
+impl PrettyDateFormatRule for JustNowPrettyDateFormatRule {
+    fn does_match(&self, date: &NaiveDateTime, reference_date: &NaiveDateTime) -> bool {
+        let time_diff = reference_date.timestamp_millis() - date.timestamp_millis();
+        time_diff < 60000
+    }
+
+    fn format<'a>(&self) -> StrftimeItems<'a> {
+        StrftimeItems::new("Just now")
+    }
 }
--- src/test.rs
+++ src/test.rs
@@ -10,9 +10,22 @@ mod tests {
                 .unwrap();
 
         let reference_date =
-            NaiveDateTime::parse_from_str("2022-09-05 23:56:04", "%Y-%m-%d %H:%M:%S")
+            NaiveDateTime::parse_from_str("2022-10-05 23:56:04", "%Y-%m-%d %H:%M:%S")
                 .unwrap();
 
         assert_eq!(date.format_pretty_with_reference(&reference_date), "5 September 2015, 23:56");
+    }
+
+    #[test]
+    fn test_just_now() {
+        let date =
+            NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S")
+                .unwrap();
+
+        let reference_date =
+            NaiveDateTime::parse_from_str("2015-09-05 23:56:05", "%Y-%m-%d %H:%M:%S")
+                .unwrap();
+
+        assert_eq!(date.format_pretty_with_reference(&reference_date), "Just now");
     }
 }
